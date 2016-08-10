689d0d4607eb2b23c2954806e97138860e4bf1aa
fjolme <fjolme@gmail.com>
2016-08-10 10:24:09 +0100

    Probando el repositorio de código

--- PruebaGit/ViewController.swift
+++ PruebaGit/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // Probando repositorio de código
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
